2bed1d556e98df3f4ebb90acc191f81a137c9bd5
dpears04 <dpears04@uoguelph.ca>
2022-12-01 00:18:53 -0500

    Day one done!

--- 2022/Day1/2022_1.py
+++ 2022/Day1/2022_1.py
@@ -4,5 +4,15 @@ from aoc import get_input
 
 data = get_input(1).splitlines()
 
+mostCal = []
+cur = 0
+
 for line in data:
-    print(line)
+    if (line == ""):
+        mostCal.append(cur)
+        cur = 0
+    else:
+        cur += int(line)
+
+mostCal.sort(reverse=True)
+print(sum(mostCal[:3]))
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 | Advent of Code 2022 | Stars   | Language |
 | ------------------- | ------- | -------- |
-| Day 1               | ? Stars | Python   |
+| Day 1               | 2 Stars | Python   |
 
 | Advent of Code 2021 | Stars   | Language |
 | ------------------- | ------- | -------- |
