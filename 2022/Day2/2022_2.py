29ae69f853529f3c97fe6eb147d85b4f9d050e74
dpears04 <dpears04@uoguelph.ca>
2022-12-02 05:34:47 +0000

    Finished day 2

--- 2022/Day2/2022_2.py
+++ 2022/Day2/2022_2.py
@@ -4,5 +4,30 @@ from aoc import get_input
 
 data = get_input(2).splitlines()
 
+score = 0
 for line in data:
-    print(line)
+    line.split(" ")
+    if "A" in line[0]:
+        if "X" in line[2]:
+            score += 0 + 3
+        elif "Y" in line[2]:
+            score += 3 + 1
+        elif "Z" in line[2]:
+            score += 6 + 2
+    elif "B" in line[0]:
+        if "X" in line[2]:
+            score += 0 + 1
+        elif "Y" in line[2]:
+            score += 3 + 2
+        elif "Z" in line[2]:
+            score += 6 + 3
+    elif "C" in line[0]:
+        if "X" in line[2]:
+            score += 0 + 2 
+        elif "Y" in line[2]:
+            score += 3 + 3
+        elif "Z" in line[2]:
+            score += 6 + 1
+    
+
+print(score)
