f5cb1f8e1ce0588b16b41778ea1d07b1dbd15979
dpears04 <dpears04@uoguelph.ca>
2022-12-07 18:32:49 +0000

    Day 7 Done and cleaned up

--- 2022/Day7/2022_7.py
+++ 2022/Day7/2022_7.py
@@ -4,48 +4,57 @@ sys.path.append("../../")
 
 from aoc import get_input
 
-
 data = get_input(7).splitlines()
-root_size = 0
+
+# P1 Vars
 res = 0
+
+# P2 Vars
+res2 = 10**10
+most = 70000000
+unused = 30000000
 
 
 class Node:
-    """Node"""
     def __init__(self, name, kind, parent):
-        self.parent = parent
         self.children = []
-        self.size = 0
+        self.files = []
+        self.parent = parent
         self.name = name
         self.kind = kind
-        self.files = []
+        self.size = 0
 
     # Insert Node
     def insert(self, node):
-        """Insert"""
         self.children.append(node)
 
     def print_tree(self):
-        """Printing"""
         print(f"{self.name} ({self.kind}) size={self.size}")
         print(f" -> {self.files} and children {len(self.children)}")
-
         for node in self.children:
             node.print_tree()
+
     def set_sums(self):
-        '''Sums of dirs'''
         self.size = sum(self.files)
         for node in self.children:
             node.set_sums()
         if self.parent != None:
             self.parent.size += self.size
-    
+
     def get_res(self):
         global res
         if self.size < 100000:
             res += self.size
         for node in self.children:
             node.get_res()
+
+    def find_smallest(self, space):
+        global res2
+        if self.size < res2 and self.size > space:
+            res2 = self.size
+        for node in self.children:
+            node.find_smallest(space)
+
 
 root = Node("/", "dir", None)
 curNode = root
@@ -65,8 +74,11 @@ for line in data:
     elif line[1] == "cd" and line[2] == "..":
         curNode = curNode.parent
 
-# Set dir sizes
 curNode = root
 root.set_sums()
 root.get_res()
+
+free_amount = unused - (most - root.size)
+root.find_smallest(free_amount)
 print(res)
+print(res2)
