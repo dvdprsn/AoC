7d92ddca54ed6e02d630a40f28f04d7c3df8b83d
dpears04 <dpears04@uoguelph.ca>
2022-12-08 18:06:12 +0000

    Done Part 2!

--- 2022/Day8/2022_8.py
+++ 2022/Day8/2022_8.py
@@ -6,50 +6,59 @@ from aoc import get_input
 data = get_input(8).splitlines()
 num_lines = sum(1 for l in data)
 num_elems = len(data[0])
-res = ((2 * num_lines) + (2 * num_elems))-4
+res = ((2 * num_lines) + (2 * num_elems)) - 4
 arr = []
 
+
 def find_right(elem, cur_row, cur_col):
-    for i in range(cur_col+1, num_elems):
+    steps = 0
+    for i in range(cur_col + 1, num_elems):
+        steps += 1
         if int(arr[cur_row][i]) >= elem:
-            return False
-    return True
+            return steps
+    return steps
+
 
 def find_up(elem, cur_row, cur_col):
-    for i in range(cur_row-1, -1, -1):
+    steps = 0
+    for i in range(cur_row - 1, -1, -1):
+        steps += 1
         if int(arr[i][cur_col]) >= elem:
-            return False
-    return True
+            return steps
+    return steps
+
 
 def find_down(elem, cur_row, cur_col):
-    for i in range(cur_row+1, num_lines):
+    steps = 0
+    for i in range(cur_row + 1, num_lines):
+        steps += 1
         if int(arr[i][cur_col]) >= elem:
-            return False
-    return True
+            return steps
+    return steps
+
 
 def find_left(elem, cur_row, cur_col):
-    for i in range(cur_col-1, -1, -1):
+    steps = 0
+    for i in range(cur_col - 1, -1, -1):
+        steps += 1
         if int(arr[cur_row][i]) >= elem:
-            return False
-    return True
+            return steps
+    return steps
 
+
+# Populate 2d array
 for i, line in enumerate(data):
     arr.append(list(line))
-# For each horizontal
+
+max_score = 0
 for i in range(1, num_lines - 1):
-    # For each vertical
     for j in range(1, num_elems - 1):
         elem = int(arr[i][j])
-        if find_right(elem,i,j):
-            res+=1
-            continue
-        if find_up(elem,i,j):
-            res+=1
-            continue
-        if find_down(elem,i,j):
-            res+=1
-            continue
-        if find_left(elem,i,j):
-            res+=1
-            continue
-print(res)
+        r = find_right(elem, i, j)
+        l = find_left(elem, i, j)
+        u = find_up(elem, i, j)
+        d = find_down(elem, i, j)
+        tot = r * l * u * d
+        if tot > max_score:
+            max_score = tot
+print(max_score)
