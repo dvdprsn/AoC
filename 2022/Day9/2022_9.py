0dfd5caf286387df7137da5e0347eb99ac86534b
dpears04 <dpears04@uoguelph.ca>
2022-12-09 16:19:21 +0000

    Done part 1

--- 2022/Day9/2022_9.py
+++ 2022/Day9/2022_9.py
@@ -5,7 +5,14 @@ from aoc import get_input
 
 data = get_input(9).splitlines()
 
-
+def is_negative(i):
+    if i == 0:
+        return 0
+    if i < 0:
+        return -1
+    else: 
+        return 1
+    
 class Rope_Point:
     def __init__(self):
         self.x_pos = 0
@@ -13,7 +20,7 @@ class Rope_Point:
         self.unique_spaces = []
 
     def is_newspace(self):
-        pos = f"{self.x_pos} + , + {self.y_pos}"
+        pos = f"{self.x_pos},{self.y_pos}"
         if pos not in self.unique_spaces:
             self.unique_spaces.append(pos)
 
@@ -32,37 +39,11 @@ def detect_move(point_head, point_tail):
     y_difference = point_head.y_pos - point_tail.y_pos
     abs_x = abs(x_difference)
     abs_y = abs(y_difference)
-    # Diagonal
-    if abs_x >= 1 and abs_y >= 1:
-        # !! TODO THIS IS WRONG, Think about cases
-        if abs_x > 1 or abs_y > 1:
-            if x_difference < 0 or y_difference < 0:
-                point_tail.move_x(-1)
-                point_tail.move_y(-1)
-            else:
-                point_tail.move_x(1)
-                point_tail.move_y(1)
-            
+
+    if abs_x == 2 or abs_y == 2:
+        tail.move_x(is_negative(x_difference))
+        tail.move_y(is_negative(y_difference))
         
-        point_tail.is_newspace()
-        return
-
-    if abs_x > 1:
-        if x_difference < -1:
-            point_tail.move_x(-1)
-        else:
-            point_tail.move_x(1)
-        point_tail.is_newspace()
-        return
-
-    if abs_y > 1:
-        if y_difference < -1:
-            point_tail.move_y(-1)
-        else:
-            point_tail.move_y(1)
-        point_tail.is_newspace()
-        return
-
     point_tail.is_newspace()
 
 
@@ -70,8 +51,6 @@ head = Rope_Point()
 tail = Rope_Point()
 for line in data:
     line = line.split(" ")
-    # print(line)
-
     for i in range(0, int(line[1])):
         if "U" in line[0]:
             head.move_x(1)
@@ -83,8 +62,7 @@ for line in data:
             head.move_y(-1)
 
         detect_move(head, tail)
-        
+
 head.print_point("head")
 tail.print_point("tail")
-
 print(len(tail.unique_spaces))
